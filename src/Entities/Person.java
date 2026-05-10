5f582a520b6698bb950c1074e1b9fedcfc5d397c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-10 09:47:31 +0400

    Class Person is created with its attributes

---
+++ src/Entities/Person.java
@@ -0,0 +1,14 @@
+package Entities;
+
+import java.time.LocalDate;
+
+public class Person {
+    String ID;
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+    String gender;
+    String phoneNumber;
+    String email;
+    String address;
+}
