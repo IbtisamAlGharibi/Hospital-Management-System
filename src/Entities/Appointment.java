89d6572946f0512492c6f4d65933e2134a704326
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-10 14:13:18 +0400

    Class Appointment is created with its attributes

---
+++ src/Entities/Appointment.java
@@ -0,0 +1,15 @@
+package Entities;
+
+import java.time.LocalDate;
+
+public class Appointment {
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private LocalDate appointmentDate;
+    private String appointmentTime;
+    private String status;
+    private String reason;
+    private String notes;
+
+}
