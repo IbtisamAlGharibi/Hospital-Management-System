fbca226053b20f33f60912465e5946b62c67ed26
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-10 14:06:06 +0400

    constructor is created

--- src/Entities/MedicalRecord.java
+++ src/Entities/MedicalRecord.java
@@ -12,5 +12,14 @@ public class MedicalRecord {
     private String testResults;
     private String notes;
 
-
+    public MedicalRecord(String recordId, String patientId, String doctorId, LocalDate visitDate, String diagnosis, String prescription, String testResults, String notes) {
+        this.recordId = recordId;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.visitDate = visitDate;
+        this.diagnosis = diagnosis;
+        this.prescription = prescription;
+        this.testResults = testResults;
+        this.notes = notes;
+    }
 }
