0a6747665ba2777a5fe563a4aae1f443de3f0c6c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-10 14:50:02 +0400

    getter ans setter is generated

--- src/Entities/Department.java
+++ src/Entities/Department.java
@@ -21,4 +21,60 @@ public class Department {
         this.availableBeds = availableBeds;
         this.bedCapacity = bedCapacity;
     }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getHeadDoctorId() {
+        return headDoctorId;
+    }
+
+    public void setHeadDoctorId(String headDoctorId) {
+        this.headDoctorId = headDoctorId;
+    }
+
+    public List<String> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(List<String> doctors) {
+        this.doctors = doctors;
+    }
+
+    public List<String> getNurses() {
+        return nurses;
+    }
+
+    public void setNurses(List<String> nurses) {
+        this.nurses = nurses;
+    }
+
+    public int getBedCapacity() {
+        return bedCapacity;
+    }
+
+    public void setBedCapacity(int bedCapacity) {
+        this.bedCapacity = bedCapacity;
+    }
+
+    public int getAvailableBeds() {
+        return availableBeds;
+    }
+
+    public void setAvailableBeds(int availableBeds) {
+        this.availableBeds = availableBeds;
+    }
 }
