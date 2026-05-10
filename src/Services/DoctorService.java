318b6d38270aa82c5dad69d3b558427c2701445e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-10 16:17:20 +0400

    addDoctor() method Done

--- src/Services/DoctorService.java
+++ src/Services/DoctorService.java
@@ -10,4 +10,41 @@ public class DoctorService {
     static Doctor doctor = new Doctor();
     static List<Doctor> doctors = new ArrayList<>();
     static Scanner scanner = new Scanner(System.in);
+
+    public void addDoctor(){
+        System.out.println("ADDING NEW DOCTOR");
+        System.out.println("----------------------------");
+
+        System.out.println("Please enter Doctor first name: ");
+        String doctorFirstName = scanner.nextLine();
+       doctor.setFirstName(doctorFirstName);
+
+        System.out.println("Please enter Doctor last name: ");
+        String doctorLastName = scanner.nextLine();
+       doctor.setLastName(doctorLastName);
+
+        System.out.println("Please enter Doctor ID: ");
+        String doctorID = scanner.nextLine();
+        doctor.setDoctorId(doctorID);
+
+        System.out.println("Please enter qualification: ");
+        String doctorQualification = scanner.nextLine();
+        doctor.setQualification(doctorQualification);
+
+        System.out.println("Please enter specialization: ");
+        String doctorSpecialization = scanner.nextLine();
+        doctor.setSpecialization(doctorSpecialization);
+
+        System.out.println("Please enter Doctor experience Years: ");
+        String doctorExperienceYears = scanner.nextLine();
+        doctor.setExperienceYears(Integer.parseInt(doctorExperienceYears));
+
+        System.out.println("Please enter department Id: ");
+        String departmentId = scanner.nextLine();
+        doctor.setDepartmentId(departmentId);
+
+        System.out.println("Please enter consultation Fee: ");
+        String consultationFee = scanner.nextLine();
+        doctor.setConsultationFee(Double.parseDouble(consultationFee));
+    }
 }
