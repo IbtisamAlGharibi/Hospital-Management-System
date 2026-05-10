1430bf18dbd4f1f99d737cdaa890f536695ab6e3
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-10 16:08:58 +0400

    new DoctorService class is created

---
+++ src/Services/DoctorService.java
@@ -0,0 +1,13 @@
+package Services;
+
+import Entities.Doctor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DoctorService {
+    static Doctor doctor = new Doctor();
+    static List<Doctor> doctors = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+}
