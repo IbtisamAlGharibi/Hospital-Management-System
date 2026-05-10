7128139aae639bf3cd7ec6ed9291bd286c375c86
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-10 12:18:20 +0400

    Class Doctor is created with its attributes and the constructor

--- src/Entities/Doctor.java
+++ src/Entities/Doctor.java
@@ -1,9 +1,10 @@
 package Entities;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Doctor{
+public class Doctor extends Person{
     private String doctorId;
     private String specialization;
     private String qualification;
@@ -13,4 +14,15 @@ public class Doctor{
     private List<Integer> availableSlots = new ArrayList<>();
     private List<String > assignedPatients = new ArrayList<>();
 
+    public Doctor(String ID, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<Integer> availableSlots, List<String> assignedPatients) {
+        super(ID, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+        this.doctorId = doctorId;
+        this.specialization = specialization;
+        this.qualification = qualification;
+        this.experienceYears = experienceYears;
+        this.departmentId = departmentId;
+        this.consultationFee = consultationFee;
+        this.availableSlots = availableSlots;
+        this.assignedPatients = assignedPatients;
+    }
 }
