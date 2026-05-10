efc4ceee5e6f41108953e3755aaf8f82f64d5c17
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-10 14:49:20 +0400

    Class Department is created with its attributes

---
+++ src/Entities/Department.java
@@ -0,0 +1,15 @@
+package Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+    private String departmentId;
+    private String departmentName;
+    private String headDoctorId;
+    private List<String > doctors =new ArrayList<>();
+    private List<String > nurses = new ArrayList<>();
+    private int bedCapacity ;
+    private int availableBeds;
+
+}
