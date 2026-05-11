6a979e4e0419811945e59eeb7d5f7bcebf14183b
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 11:00:46 +0400

    addNurse() method is created

--- src/Services/NurseService.java
+++ src/Services/NurseService.java
@@ -2,11 +2,43 @@ package Services;
 
 import Entities.Nurse;
 
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class NurseService {
     static Scanner scanner = new Scanner(System.in);
     static Nurse nurse = new Nurse();
 
+    public Nurse addNurse(){
+        System.out.println("ADDING NEW NURSE");
+        System.out.println("----------------------------");
+
+        System.out.println("Please enter nurse first name: ");
+        String nurseFirstName = scanner.nextLine();
+        nurse.setFirstName(nurseFirstName);
+
+        System.out.println("Please enter nurse last name: ");
+        String nurseLastName = scanner.nextLine();
+        nurse.setLastName(nurseLastName);
+
+        System.out.println("Please enter nurse Id: ");
+        String nurseId = scanner.nextLine();
+        nurse.setNurseId(nurseId);
+
+        System.out.println("Please enter department Id: ");
+        String departmentId = scanner.nextLine();
+        nurse.setDepartmentId(departmentId);
+
+        System.out.println("Please enter shift (Morning/Evening/Night): ");
+        String shift = scanner.nextLine();
+        nurse.setShift(shift);
+
+        System.out.println("Please enter qualification: ");
+        String qualification = scanner.nextLine();
+        nurse.setQualification(qualification);
+
+        return nurse;
+    }
 
 }
