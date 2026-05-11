1201f640f90fb6afdd6d6c51ea6f45dd617f970b
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 14:04:27 +0400

    performSurgery() method Done

--- src/Entities/Surgeon.java
+++ src/Entities/Surgeon.java
@@ -3,11 +3,13 @@ package Entities;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Surgeon extends Doctor{
     private int surgeriesPerformed;
     private List<String> surgeryTypes = new ArrayList<>();
     private boolean operationTheatreAccess;
+    static Scanner scanner = new Scanner(System.in);
 
     public Surgeon(String ID, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients, int surgeriesPerformed, List<String> surgeryTypes, boolean operationTheatreAccess) {
         super(ID, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
@@ -66,5 +68,17 @@ public class Surgeon extends Doctor{
         System.out.println("surgeries Performed : " + getSurgeriesPerformed());
         System.out.println("surgery Types : " + getSurgeryTypes());
 
+    }
+
+    public void performSurgery(){
+        if (!operationTheatreAccess) {
+            System.out.println("No Operation Theatre Access");
+        }else {
+            System.out.println("Please enter surgery type:");
+            String surgeryType = scanner.nextLine();
+            System.out.println(surgeryType + " " + "Surgery performed");
+            surgeriesPerformed++;
+            surgeryTypes.add(surgeryType);
+        }
     }
 }
