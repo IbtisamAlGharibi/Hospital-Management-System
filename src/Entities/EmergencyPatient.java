c54fc618bf76036523045177cfaf69b27852c0c1
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 16:08:28 +0400

    class EmergencyPatient is added with its attributes

--- src/Entities/EmergencyPatient.java
+++ src/Entities/EmergencyPatient.java
@@ -1,4 +1,9 @@
 package Entities;
 
 public class EmergencyPatient {
+    private String emergencyType;
+    private String arrivalMode;
+    private int triageLevel;
+    private boolean admittedViaER;
+
 }
