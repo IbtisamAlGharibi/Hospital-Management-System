ee134dde2874de6dc6f4d93489c2ac46d6fc8c95
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 11:53:37 +0400

    MedicalRecordService class is created

---
+++ src/Services/MedicalRecordService.java
@@ -0,0 +1,10 @@
+package Services;
+
+import Entities.MedicalRecord;
+
+public class MedicalRecordService {
+    static MedicalRecord medicalRecord = new MedicalRecord();
+    static PatientService patientService = new PatientService();
+    static DoctorService doctorService = new DoctorService();
+
+}
