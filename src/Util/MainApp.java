003f5bdd345be0ab6f4e717fa6a6a79ded372c8c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 09:27:53 +0400

    add the mainApp to do test

---
+++ src/Util/MainApp.java
@@ -0,0 +1,19 @@
+package Util;
+
+import Entities.Doctor;
+import Entities.Patient;
+import Services.DoctorService;
+import Services.PatientService;
+
+import java.util.Scanner;
+
+public class MainApp {
+    static Scanner scanner = new Scanner(System.in);
+    static DoctorService doctorService = new DoctorService();
+    static PatientService patientService = new PatientService();
+    static Patient patient = new Patient();
+    static Doctor doctor = new Doctor();
+    public static void main(String[] args){
+
+    }
+}
