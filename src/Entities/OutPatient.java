4b51198155a7a4bd31dba60948038384ebc7c8b8
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 15:37:23 +0400

    constructor is added

--- src/Entities/OutPatient.java
+++ src/Entities/OutPatient.java
@@ -1,10 +1,23 @@
 package Entities;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class OutPatient extends Patient{
     private int visitCount;
     private LocalDate lastVisitDate;
     private String preferredDoctorId;
 
+    public OutPatient(int visitCount, LocalDate lastVisitDate, String preferredDoctorId) {
+        this.visitCount = visitCount;
+        this.lastVisitDate = lastVisitDate;
+        this.preferredDoctorId = preferredDoctorId;
+    }
+
+    public OutPatient(String ID, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String patientId, String bloodGroup, List<String> allergies, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> medicalRecords, List<String> appointments, int visitCount, LocalDate lastVisitDate, String preferredDoctorId) {
+        super(ID, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, patientId, bloodGroup, allergies, emergencyContact, registrationDate, insuranceId, medicalRecords, appointments);
+        this.visitCount = visitCount;
+        this.lastVisitDate = lastVisitDate;
+        this.preferredDoctorId = preferredDoctorId;
+    }
 }
