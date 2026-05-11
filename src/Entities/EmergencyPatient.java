dc8c1be587bdb9faee9083246ab471305bb4840b
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 16:09:18 +0400

    getter and setter is added

--- src/Entities/EmergencyPatient.java
+++ src/Entities/EmergencyPatient.java
@@ -12,4 +12,36 @@ public class EmergencyPatient {
         this.triageLevel = triageLevel;
         this.admittedViaER = admittedViaER;
     }
+
+    public String getEmergencyType() {
+        return emergencyType;
+    }
+
+    public void setEmergencyType(String emergencyType) {
+        this.emergencyType = emergencyType;
+    }
+
+    public String getArrivalMode() {
+        return arrivalMode;
+    }
+
+    public void setArrivalMode(String arrivalMode) {
+        this.arrivalMode = arrivalMode;
+    }
+
+    public int getTriageLevel() {
+        return triageLevel;
+    }
+
+    public void setTriageLevel(int triageLevel) {
+        this.triageLevel = triageLevel;
+    }
+
+    public boolean isAdmittedViaER() {
+        return admittedViaER;
+    }
+
+    public void setAdmittedViaER(boolean admittedViaER) {
+        this.admittedViaER = admittedViaER;
+    }
 }
