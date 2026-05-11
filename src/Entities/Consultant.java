cf1187a63de0c970cc611a204f0964d8d270cfb9
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 14:12:08 +0400

    constructor is added

--- src/Entities/Consultant.java
+++ src/Entities/Consultant.java
@@ -1,5 +1,6 @@
 package Entities;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,4 +9,16 @@ public class Consultant extends Doctor{
     private boolean onlineConsultationAvailable;
     private int consultationDuration;
 
+    public Consultant(String ID, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients, List<String> consultationTypes, boolean onlineConsultationAvailable, int consultationDuration) {
+        super(ID, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.consultationTypes = consultationTypes;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+        this.consultationDuration = consultationDuration;
+    }
+
+    public Consultant(List<String> consultationTypes, boolean onlineConsultationAvailable, int consultationDuration) {
+        this.consultationTypes = consultationTypes;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+        this.consultationDuration = consultationDuration;
+    }
 }
