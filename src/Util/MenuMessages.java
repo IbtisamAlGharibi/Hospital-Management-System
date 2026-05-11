4e07d932348d5ebf506bcb2b0bd69d59795b3c57
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 09:22:27 +0400

    add the menu messages

---
+++ src/Util/MenuMessages.java
@@ -0,0 +1,26 @@
+package Util;
+
+public class MenuMessages {
+    public static String MAIN_MENU_MESSAGE ="1. Patient Management\n" +
+            "1.1 Register New Patient \n" +
+            "1.2 Register InPatient \n" +
+            "1.3 Register OutPatient \n" +
+            "1.4 Register Emergency Patient \n" +
+            "1.5 View All Patients \n" +
+            "1.6 Search Patient \n" +
+            "1.7 Update Patient Information \n" +
+            "1.8 Remove Patient \n" +
+            "1.9 View Patient Medical History" +
+            "2. Doctor Management \n" +
+            "2.1 Add Doctor \n" +
+            "2.2 Add Surgeon \n" +
+            "2.3 Add Consultant \n" +
+            "2.4 Add General Practitioner \n" +
+            "2.5 View All Doctors \n" +
+            "2.6 Search Doctor by Specialization \n" +
+            "2.7 View Available Doctors \n" +
+            "2.8 Assign Patient to Doctor \n" +
+            "2.9 Update Doctor Information \n" +
+            "2.10 Remove Doctor";
+
+}
