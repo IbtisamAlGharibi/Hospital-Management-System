0a11d13721c17ff07501c92bd4c6cf5b899fe9a3
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 14:11:32 +0400

    class Consultant is added with its attributes

---
+++ src/Entities/Consultant.java
@@ -0,0 +1,11 @@
+package Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Consultant extends Doctor{
+    private List<String > consultationTypes = new ArrayList<>();
+    private boolean onlineConsultationAvailable;
+    private int consultationDuration;
+
+}
