ac4fa3ce4e624bc1ecf76e68ad242e6283f45e46
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 15:10:15 +0400

    displayInfo() method Done

--- src/Entities/InPatient.java
+++ src/Entities/InPatient.java
@@ -77,4 +77,31 @@ public class InPatient extends  Patient{
     public void setDailyCharges(double dailyCharges) {
         this.dailyCharges = dailyCharges;
     }
+
+    @Override
+    public void displayInfo(){
+        System.out.println("ID : " + getID() );
+        System.out.println("First Name: " + getFirstName() );
+        System.out.println("Last Name: " + getLastName() );
+        System.out.println("Date of birth: " + getDateOfBirth() );
+        System.out.println("Gender: " + getGender() );
+        System.out.println("Phone Number: " + getPhoneNumber() );
+        System.out.println("Email: " + getEmail() );
+        System.out.println("Address: " + getAddress());
+        System.out.println("patient Id : " + getPatientId() );
+        System.out.println("blood Group : " + getBloodGroup() );
+        System.out.println("allergies : " + getAllergies() );
+        System.out.println("registrationDate : " + getRegistrationDate() );
+        System.out.println("insurance Id : " + getInsuranceId());
+        System.out.println("medical Records : " + getMedicalRecords());
+        System.out.println("appointments : " + getAppointments());
+
+        System.out.println("admission Date : " + getAdmissionDate());
+        System.out.println("discharge Date : " + getDischargeDate());
+        System.out.println("room Number : " + getRoomNumber());
+        System.out.println("bedNumber : " + getBedNumber());
+        System.out.println("admitting Doctor Id : " + getAdmittingDoctorId());
+        System.out.println("dailyCharges : " + getDailyCharges());
+
+    }
 }
