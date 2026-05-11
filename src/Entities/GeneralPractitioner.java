83f426f4c0a6c3094f6921d93a96774896c4488f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 14:36:15 +0400

    constructor is added

--- src/Entities/GeneralPractitioner.java
+++ src/Entities/GeneralPractitioner.java
@@ -1,8 +1,23 @@
 package Entities;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class GeneralPractitioner extends Doctor{
     private boolean walkinAvailable;
     private boolean homeVisitAvailable;
     private boolean vaccinationCertified;
 
+    public GeneralPractitioner(String ID, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients, boolean walkinAvailable, boolean homeVisitAvailable, boolean vaccinationCertified) {
+        super(ID, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.walkinAvailable = walkinAvailable;
+        this.homeVisitAvailable = homeVisitAvailable;
+        this.vaccinationCertified = vaccinationCertified;
+    }
+
+    public GeneralPractitioner(boolean walkinAvailable, boolean homeVisitAvailable, boolean vaccinationCertified) {
+        this.walkinAvailable = walkinAvailable;
+        this.homeVisitAvailable = homeVisitAvailable;
+        this.vaccinationCertified = vaccinationCertified;
+    }
 }
