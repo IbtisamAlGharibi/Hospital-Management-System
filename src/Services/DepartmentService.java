6d0c2316c134bf6fc7c4b6b7cc04c2b76d066ffb
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 12:12:07 +0400

    static Scanner scanner = new Scanner(System.in); class is created

---
+++ src/Services/DepartmentService.java
@@ -0,0 +1,19 @@
+package Services;
+
+import Entities.Appointment;
+import Entities.Doctor;
+import Entities.Nurse;
+import Entities.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DepartmentService {
+    static Scanner scanner = new Scanner(System.in);
+    static List<Patient> patients = new ArrayList<>();
+    static List<Doctor> doctors = new ArrayList<>();
+    static Nurse nurse = new Nurse();
+    static Doctor doctor = new Doctor();
+
+}
