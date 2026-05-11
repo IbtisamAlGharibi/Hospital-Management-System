495f72838ef1332fa51976b3240253fb8fcddd40
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 14:35:55 +0400

    class GeneralPractitioner is added with its attributes

---
+++ src/Entities/GeneralPractitioner.java
@@ -0,0 +1,8 @@
+package Entities;
+
+public class GeneralPractitioner extends Doctor{
+    private boolean walkinAvailable;
+    private boolean homeVisitAvailable;
+    private boolean vaccinationCertified;
+
+}
