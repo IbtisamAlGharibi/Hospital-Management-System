0cf0646c1112947ec71452c10c029a4dca04c2d3
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-11 11:58:46 +0400

    getRecordsByDoctorId() method is created

--- src/Services/MedicalRecordService.java
+++ src/Services/MedicalRecordService.java
@@ -1,5 +1,6 @@
 package Services;
 
+import Entities.Doctor;
 import Entities.MedicalRecord;
 import Entities.Patient;
 
@@ -13,7 +14,7 @@ public class MedicalRecordService {
     static DoctorService doctorService = new DoctorService();
     static Scanner scanner = new Scanner(System.in);
     static List<Patient> patients = new ArrayList<>();
-
+    static List<Doctor> doctors = new ArrayList<>();
 
     public void getRecordsByPatientId(){
         System.out.println("Please enter the patient Id");
@@ -21,6 +22,19 @@ public class MedicalRecordService {
         for (int i =0; i < patients.size();i++){
             String searchedId = String.valueOf(patients.get(i));
             if (patientId.equalsIgnoreCase(searchedId)){
+                medicalRecord.displayInfo();
+            }else {
+                System.out.println("NOT FOUND");
+            }
+        }
+    }
+
+    public void getRecordsByDoctorId(){
+        System.out.println("Please enter the doctor Id:");
+        String doctorId = scanner.nextLine();
+        for (int i =0; i < doctors.size();i++){
+            String searchedId = String.valueOf(doctors.get(i));
+            if (doctorId.equalsIgnoreCase(searchedId)){
                 medicalRecord.displayInfo();
             }else {
                 System.out.println("NOT FOUND");
