e36c4a252e5a632e923b6643048e239f6dee230a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-12 15:19:57 +0400

    addition in EmergencyPatient class

--- src/Entities/EmergencyPatient.java
+++ src/Entities/EmergencyPatient.java
@@ -2,6 +2,12 @@ package Entities;
 
 import Behaviours.DisplayableInterface;
 
+import java.time.LocalDate;
+import java.util.Collections;
+
+import static Entities.Doctor.patient;
+import static Services.PatientService.patients;
+
 public class EmergencyPatient extends Patient implements DisplayableInterface {
     private String emergencyType;
     private String arrivalMode;
@@ -76,5 +82,78 @@ public class EmergencyPatient extends Patient implements DisplayableInterface {
     @Override
     public void displaySummary() {
 
+    }
+
+   /* @Override
+    public Patient addPatient() {
+        return null;
+    }*/
+
+    @Override
+    public Patient addPatient() {
+        System.out.println("ADDING NEW  INPATIENT");
+        System.out.println("----------------------------");
+
+        System.out.println("Please enter patient first name: ");
+        String patientFirstName = scanner.nextLine();
+        setFirstName(patientFirstName);
+
+        System.out.println("Please enter patient last name: ");
+        String patientLastName = scanner.nextLine();
+        setLastName(patientLastName);
+
+        System.out.println("Please enter patient ID: ");
+        String patientID = scanner.nextLine();
+        setPatientId(patientID);
+
+        System.out.println("Please enter blood Group: ");
+        String patientBloodGroup = scanner.nextLine();
+        setBloodGroup(patientBloodGroup);
+
+        System.out.println("Please enter emergency Contact: ");
+        String patientEmergencyContact = scanner.nextLine();
+        setEmergencyContact(patientEmergencyContact);
+
+        System.out.println("Please enter insurance Id: ");
+        String patientInsuranceId = scanner.nextLine();
+        setInsuranceId(patientInsuranceId);
+
+        System.out.println("Please enter allergies: ");
+        String patientAllergies = scanner.nextLine();
+        setAllergies(Collections.singletonList(patientAllergies));
+
+        System.out.println("Please enter registrationDate: ");
+        String patientRegistrationDate = scanner.nextLine();
+        setRegistrationDate(LocalDate.parse(patientRegistrationDate));
+
+        System.out.println("Please enter emergency Type: ");
+        String emergencyType = scanner.nextLine();
+        setEmergencyType(emergencyType);
+
+        System.out.println("Please enter arrival Mode: ");
+        String arrivalMode = scanner.nextLine();
+       setArrivalMode(arrivalMode);
+
+        System.out.println("Please enter triage Level: ");
+        String triageLevel = scanner.nextLine();
+       setTriageLevel(Integer.parseInt(triageLevel));
+
+        System.out.println("Please enter admitted Via ER(True/False): ");
+        String admittedViaER = scanner.nextLine();
+        setAdmittedViaER(Boolean.parseBoolean(admittedViaER));
+        return patient;
+
+    }
+
+    public void addNewPatient() {
+        boolean flag = true;
+        while (flag) {
+            patients.add(addPatient());
+            System.out.println("Please enter E to Stop");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("E")) {
+                flag = false;
+            }
+        }
     }
 }
