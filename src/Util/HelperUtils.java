12bb79c3fe4815eca16138a72c2d59bab022af07
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-12 12:19:18 +0400

    generateId(String prefix) method Done

--- src/Util/HelperUtils.java
+++ src/Util/HelperUtils.java
@@ -1,5 +1,6 @@
 package Util;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class HelperUtils {
@@ -57,5 +58,10 @@ public class HelperUtils {
     }
     public String generateId(){
         return UUID.randomUUID().toString();
+    }
+    public String generateId(String prefix){
+        Random random = new Random();
+        int number = 10000 + random.nextInt(90000);
+        return prefix + "-" + number;
     }
 }
