a20184598798c581b9492de75c2da50576106d6c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-13 11:20:40 +0400

    ReportsService is created to handle the menu

---
+++ src/Services/ReportsService.java
@@ -0,0 +1,37 @@
+package Services;
+
+import Util.MenuMessages;
+
+import java.util.Scanner;
+
+public class ReportsService {
+    static MenuMessages menuMessages = new MenuMessages();
+    static Scanner scanner = new Scanner(System.in);
+
+    public boolean HandleReportsMenu(){
+        System.out.println(menuMessages.REPORTS_MENU_MESSAGE);
+        System.out.println("Please enter number");
+        String reportOption = scanner.nextLine();
+        switch (reportOption) {
+            case  "7.1" -> {
+
+            }
+            case "7.2" -> {
+
+            }
+            case "7.3" -> {
+
+            }
+
+            case  "7.4" -> {
+
+            }
+            case "7.5" -> {
+            }
+            case "7.6" -> {
+                return false;
+            }
+        }
+        return true;
+    }
+}
