b5acd482d84aa6183b9dc1d24b94d7bd64e04180
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-13 08:53:38 +0400

    Adding case 3.3

--- src/Services/NurseService.java
+++ src/Services/NurseService.java
@@ -176,7 +176,8 @@ public class NurseService implements ManageableInterface, SearchableInterface {
                 displayAllNurses();
             }
             case "3.3" -> {
-
+                System.out.println("VIEWING NURSE BU DEPARTMENT");
+                getNursesByDepartment();
             }
 
             case  "3.4" -> {
