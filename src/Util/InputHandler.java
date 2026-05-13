395e0ab2b064a3b73e44c9ab20479aee1ad38878
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-13 11:46:41 +0400

    getIntInput(String prompt, int min, int max) Done

--- src/Util/InputHandler.java
+++ src/Util/InputHandler.java
@@ -13,4 +13,21 @@ public class InputHandler {
         Scanner scanner = new Scanner(System.in);
         prompt = String.valueOf(scanner.nextInt());
     }
+
+    public int getIntInput(String prompt, int min, int max){
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        while (true) {
+
+            System.out.print(prompt);
+            number = scanner.nextInt();
+
+            if (number >= min && number <= max) {
+                return number;
+            }
+
+            System.out.println("Invalid number. Try again.");
+        }
+    }
+
 }
