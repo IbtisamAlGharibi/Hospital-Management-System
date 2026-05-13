a99e00629d51c3409ec7c7c0d97d2272881ad64d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-13 11:42:04 +0400

    getStringInput(String prompt) Done

--- src/Util/InputHandler.java
+++ src/Util/InputHandler.java
@@ -1,5 +1,11 @@
 package Util;
 
+import java.util.Scanner;
+
 public class InputHandler {
 
+    public void getStringInput(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        prompt = scanner.nextLine();
+    }
 }
