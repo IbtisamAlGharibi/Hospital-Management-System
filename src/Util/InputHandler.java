1ea228f8d770890f080de0d662b4fc81ded67e52
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-13 11:42:58 +0400

    getIntInput(String prompt) Done

--- src/Util/InputHandler.java
+++ src/Util/InputHandler.java
@@ -8,4 +8,9 @@ public class InputHandler {
         Scanner scanner = new Scanner(System.in);
         prompt = scanner.nextLine();
     }
+
+    public void getIntInput(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        prompt = String.valueOf(scanner.nextInt());
+    }
 }
